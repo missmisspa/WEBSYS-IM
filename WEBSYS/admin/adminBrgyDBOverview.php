042d6aaea7c1bb541e96bc9d08aeb9b8498a38c3
Christian Ballon <christiandaneballon@gmail.com>
2024-05-19 01:13:39 +0800

    full functioning db overview, changed html to php 	modified:   WEBSYS/admin/adminBrgyDBOverview.php

--- WEBSYS/admin/adminBrgyDBOverview.php
+++ WEBSYS/admin/adminBrgyDBOverview.php
@@ -1,3 +1,38 @@
+<?php
+session_start();
+
+include("../connection.php");
+include("../function.php");
+
+$user_data = check_login($con);
+
+// SQL queries to fetch data
+$total_population_query = "SELECT COUNT(*) AS total FROM resident_info";
+$senior_citizen_query = "SELECT COUNT(*) AS total FROM resident_info WHERE resi_age >= 60";
+$adult_query = "SELECT COUNT(*) AS total FROM resident_info WHERE resi_age BETWEEN 18 AND 59";
+$youth_query = "SELECT COUNT(*) AS total FROM resident_info WHERE resi_age BETWEEN 13 AND 17";
+$children_query = "SELECT COUNT(*) AS total FROM resident_info WHERE resi_age < 13";
+$male_query = "SELECT COUNT(*) AS total FROM resident_info WHERE resi_sex = 'Male'";
+$female_query = "SELECT COUNT(*) AS total FROM resident_info WHERE resi_sex = 'Female'";
+
+// Fetch data from database
+$total_population_result = mysqli_query($con, $total_population_query);
+$senior_citizen_result = mysqli_query($con, $senior_citizen_query);
+$adult_result = mysqli_query($con, $adult_query);
+$youth_result = mysqli_query($con, $youth_query);
+$children_result = mysqli_query($con, $children_query);
+$male_result = mysqli_query($con, $male_query);
+$female_result = mysqli_query($con, $female_query);
+
+// Fetch associative arrays
+$total_population = mysqli_fetch_assoc($total_population_result)['total'];
+$senior_citizen = mysqli_fetch_assoc($senior_citizen_result)['total'];
+$adult = mysqli_fetch_assoc($adult_result)['total'];
+$youth = mysqli_fetch_assoc($youth_result)['total'];
+$children = mysqli_fetch_assoc($children_result)['total'];
+$male = mysqli_fetch_assoc($male_result)['total'];
+$female = mysqli_fetch_assoc($female_result)['total'];
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -38,72 +73,82 @@
             <a class="nav-link" href="adminAbout.html"><i class="fas fa-info-circle"></i> About</a>
         </li>
         <li class="nav-item">
-            <a class="nav-link logout" href="#" onclick="confirmLogout()"><i class="fas fa-sign-out-alt"></i> Logout</a>
+            <button type="submit" name="logout" class="nav-link logout" onclick="confirmLogout()" 
+            style="border: none;
+                            cursor: pointer;
+                            transition: all 0.3s ease;
+                            background-color: #748C70;
+                            color: #FFFFFF;
+                            border-radius:10px; 
+                            border:none;
+                            transition: all 0.3s ease;
+                            padding-left:30px;
+                            padding-right:30px;
+                            margin-left: 25px;"   
+                            onmouseover="this.style.backgroundColor='#5F775B'; this.style.transform='scale(1.1)';" 
+                            onmouseout="this.style.backgroundColor='#748C70'; this.style.transform='scale(1)';"
+                            ><i class="fas fa-sign-out-alt"></i> Logout</button>
         </li>
     </ul>
-    
-    
 </div>
 
-    <div id="content">
-        <h2>Barangay Database</h2>
-        <div id="container">
-                <div class="container mt-5">
-                    <div class="row">
-                        <div class="col">
-                            <button class="custom-btn btn-color1"><span class="btn-text">Total Population</span>
-                                <p class="text-center mt-2">521</p>
-                            </button>
-                            
-                        </div>
-                        <div class="col">
-                            <button class="custom-btn btn-color2"><span class="btn-text">Senior Citizen</span>
-                                <p class="text-center mt-2">56</p>
-                            </button>
-                        </div>
-                        <div class="col">
-                            <button class="custom-btn btn-color3"><span class="btn-text">Adult</span>
-                                <p class="text-center mt-2">124</p>
-                            </button>
-                        </div>
-                    </div>
-                    <div class="row mt-3">
-                        <div class="col">
-                            <button class="custom-btn btn-color4"><span class="btn-text">Youth</span>
-                                <p class="text-center mt-2">245</p>
-                            </button>
-                        </div>
-                        <div class="col">
-                            <button class="custom-btn btn-color5"><span class="btn-text">Children</span>
-                                <p class="text-center mt-2">145</p>
-                            </button>
-                        </div>
-                        <div class="col">
-                            <button class="custom-btn btn-color6"><span class="btn-text">Male</span>
-                                <p class="text-center mt-2">245</p>
-                            </button>
-                        </div>
-                        </div>
-                    <div class="row mt-3">
-                        <div class="col">
-                            <button class="custom-btn btn-color7"><span class="btn-text">Female</span>
-                                <p class="text-center mt-2">325</p>
-                            </button>
-                        </div>
-                    </div>
+<div id="content">
+    <h2>Barangay Database</h2>
+    <div id="container">
+        <div class="container mt-5">
+            <div class="row">
+                <div class="col">
+                    <button class="custom-btn btn-color1"><span class="btn-text">Total Population</span>
+                        <p class="text-center mt-2"><?php echo $total_population; ?></p>
+                    </button>
                 </div>
-        </div>
-        <div class="button-container">
-            <a href="./adminBrgyDBSearch.html" class="btn-custom" id="search-btn">Search Database</a>
+                <div class="col">
+                    <button class="custom-btn btn-color2"><span class="btn-text">Senior Citizen</span>
+                        <p class="text-center mt-2"><?php echo $senior_citizen; ?></p>
+                    </button>
+                </div>
+                <div class="col">
+                    <button class="custom-btn btn-color3"><span class="btn-text">Adult</span>
+                        <p class="text-center mt-2"><?php echo $adult; ?></p>
+                    </button>
+                </div>
+            </div>
+            <div class="row mt-3">
+                <div class="col">
+                    <button class="custom-btn btn-color4"><span class="btn-text">Youth</span>
+                        <p class="text-center mt-2"><?php echo $youth; ?></p>
+                    </button>
+                </div>
+                <div class="col">
+                    <button class="custom-btn btn-color5"><span class="btn-text">Children</span>
+                        <p class="text-center mt-2"><?php echo $children; ?></p>
+                    </button>
+                </div>
+                <div class="col">
+                    <button class="custom-btn btn-color6"><span class="btn-text">Male</span>
+                        <p class="text-center mt-2"><?php echo $male; ?></p>
+                    </button>
+                </div>
+            </div>
+            <div class="row mt-3">
+                <div class="col">
+                    <button class="custom-btn btn-color7"><span class="btn-text">Female</span>
+                        <p class="text-center mt-2"><?php echo $female; ?></p>
+                    </button>
+                </div>
+            </div>
         </div>
     </div>
+    <div class="button-container">
+        <a href="./adminBrgyDBSearch.html" class="btn-custom" id="search-btn">Search Database</a>
+    </div>
+</div>
 
-        
 <div class="popup" id="popup">
     <div class="popup-content">
         <p>Are you sure you want to logout?</p>
         <button id="cancelButton" onclick="hidePopup()">Cancel</button>
-        <button id="logoutBtn" onclick="logout()">Logout</button>
+        <a href="../logout.php"><button id="logoutBtn" onclick="logout()">Logout</button></a>
     </div>
 </div>
 
@@ -119,13 +164,7 @@
     function confirmLogout() {
         showPopup();
     }
-
-    function logout() {
-        hidePopup();
-        window.location.href = '../login1.html'; 
-    }
 </script>  
-
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
